85bbf4be14dedf650d58ef638fd2ec8b0f24836e
taki1824 <sin@sidehill.jp>
2021-08-16 19:03:55 +1200

    bug fixed

---
+++ src/popup.js
@@ -0,0 +1,11 @@
+var label = null;
+var slider = null;
+window.onload = function() {
+    label = document.getElementById('label');
+    slider = document.getElementById('slider');
+
+    slider.addEventListener("input", function() {
+        label.textContent = 'Input area height is ' + slider.value + 'px';
+    });
+    chrome.runtime,sendMessage(slider.value, function(response) {})
+};
