f3a90fd589db2fb0d367efe2b8f9f6e454ea5936
taki1824 <sin@sidehill.jp>
2021-08-16 19:07:49 +1200

    v0.0.3

--- src/content.js
+++ src/content.js
@@ -1,25 +1,18 @@
 window.addEventListener ("load", myMain, false);
 
+//ページ読み込み完了後（JS処理完了後）に変数の定義＆テキストエリアの変更
 function myMain (evt) {
     var jsInitChecktimer = setInterval (checkForJS_Finish, 3000);
 
     function checkForJS_Finish () {
         if ( document.querySelector (".chat-room-messages")) {
             clearInterval (jsInitChecktimer);
+            //変数の宣言
+            textarea = document.getElementsByClassName('composer-textarea qa-test-message-composer-textarea ')[0];
+            message_area = document.getElementsByClassName('chat-message-conversation-list-container')[0];
+            height = '100px';           
             
-            console.log("test");
-
-            var textarea = document.getElementsByClassName('composer-textarea qa-test-message-composer-textarea ')[0];
-            var message_area = document.getElementsByClassName('chat-message-conversation-list-container')[0];
-            var height = '100px';
-            //var document_height = document.documentElement.clientHeight;
-        
-            chrome.runtime.onMessage.addListener(function (message) {
-                height = message + 'px';
-                return true;
-            });
-        
-            //textarea.style.minHeight = '110px';
+            
             textarea.style.maxHeight = 'unset'; 
             textarea.style.height = height;
         
@@ -27,3 +20,12 @@ function myMain (evt) {
         }
     }
 }
+
+//popupからメッセージを受け取った時の動作
+chrome.runtime.onMessage.addListener((request, sender) => {
+    console.log('呼び出されたわ！受信したデータは', request.data);
+    height = request.data + 'px';
+    
+    textarea.style.height = height;
+    message_area.style.height = 'calc(100vh - 217px - ' + height + ')';
+});
--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,6 @@
 var label = null;
 var slider = null;
+
 window.onload = function() {
     label = document.getElementById('label');
     slider = document.getElementById('slider');
@@ -7,9 +8,14 @@ window.onload = function() {
     slider.addEventListener("input", function() {
         label.textContent = 'Input area height is ' + slider.value + 'px';
 
-        chrome.runtime.sendMessage(slider.value, function(response) {
-            console.log(slider.value + "px");
-        })
+        //リファレンス https://kajindowsxp.com/chrome-message/#toc1, https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/tabs/sendMessage, https://developer.chrome.com/docs/extensions/reference/tabs/ 
+        //chrome.tabs.query() 関数で現在開いているタブを取得
+        var send_data = { type: 'height', data: slider.value };
+        chrome.tabs.query({ active: true, currentWindow: true }, aim_message);
+        function aim_message(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, send_data, );
+        }        
+        
     });
 
 };
